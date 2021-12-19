27598584105fc54bc76d78c76aeebf628ffb3737
MarcosTypeAP <sombromar@gmail.com>
2021-12-19 12:03:06 -0300

    setup script upgraded

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 
 import os
 import platform
+import sys
 
 
 def install_dependencies(pip):
@@ -15,21 +16,52 @@ def install_dependencies(pip):
     os.system(f'{pip} --quiet install -r requirements/{file_name}')
 
 
+def print_help():
+    """Prints the docs for this script."""
+    print(
+        """
+        Quick setup and start-up script for the project.
+
+        -l, --listen    Specify the port and address on which the server will listen.
+                        Eg: 0.0.0.0:8000 or localhost:8080
+
+        -h, --help      Prints this text.
+        """
+    )
+
+
 def main():
+    if len(sys.argv) == 1:
+        address = '0.0.0.0:8000'
+    elif sys.argv[1] == '-h' or sys.argv[1] == '--help':
+        print_help()
+        return 0
+    elif sys.argv[1] == '-l' or sys.argv[1] == '--listen':
+        try:
+            address = sys.argv[2]
+        except IndexError:
+            print('You must specify an address.')
+            return 0
+    else:
+        print('Arguments not recognized')
+        return 0
+
     if not os.path.exists('static'):
         os.mkdir('static')
+
     if platform.system() == 'Windows':
         python = 'python'
         pip = 'pip'
     else:
         python = 'python3'
         pip = 'pip3'
+
     install_dependencies(pip)
     os.system(f'{python} manage.py collectstatic --no-input')
     os.system(f'{python} manage.py makemigrations')
     os.system(f'{python} manage.py migrate')
-    os.system(f'{python} manage.py runserver 0.0.0.0:8000')
+    os.system(f'{python} manage.py runserver {address}')
 
 
 if __name__ == "__main__":
-   main()
+    main()
